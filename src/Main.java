37f09722df70d9cf0fe97c5a1478ef49c8d15a03
takahashi <16110013@14DELL-59.OIC_LAN1>
2018-09-07 14:20:06 +0900

    Main 作成

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//
+		//Yatagarasu yataA = new Yatagarasu();
+		//Yatagarasu yataB = new Yatagarasu();
+
+		//Zombie zomA = new Zombie();
+
+		//for(;;){
+			//zomA attack
+
+			//if 死亡 おわり
+
+		//};
+	}
+
+}
