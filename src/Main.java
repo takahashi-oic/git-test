ee1a81155d88756be028ca6eb80731aac66e5035
takahashi <16110013@14DELL-59.OIC_LAN1>
2018-09-07 14:33:24 +0900

    Main update

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		//
-		//Yatagarasu yataA = new Yatagarasu();
-		//Yatagarasu yataB = new Yatagarasu();
+		Yatagarasu yataA = new Yatagarasu();
+		Yatagarasu yataB = new Yatagarasu();
 
-		//Zombie zomA = new Zombie();
+		Zombie zomA = new Zombie();
 
-		//for(;;){
-			//zomA attack
-
-			//if 死亡 おわり
-
-		//};
+		for(;;){
+			zomA.attack(yataB);
+			//死んだらおわり
+			if(yataB.getHp <= 0){
+				return;
+			}
+		};
 	}
-
 }
