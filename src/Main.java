7c3d625f19f683835f26826aabc03fc878849541
takahashi <16110013@14DELL-59.OIC_LAN1>
2018-09-10 11:00:04 +0900

    Main update

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,10 @@ public class Main {
 
 		Zombie zomA = new Zombie(10);
 
+		yataA.setHp(20);
+		yataB.setHp(20);
+		zomA.setHp(20);
+
 		for(;;){
 			System.out.println("ゾンビAが八咫烏Bに攻撃");
 			zomA.attack(yataB);
