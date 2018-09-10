27be5c99ebfd5b1f01eefc1eccddd0bd03834eef
koyama <kym1290kh@gmail.com>
2018-09-10 13:00:30 +0900

    Main update

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,19 @@ public class Main {
 		Yatagarasu yataA = new Yatagarasu();
 		Yatagarasu yataB = new Yatagarasu();
 
+		Hero fujii = new Hero();
+		Wizard ito = new Wizard();
+
+		Weapon w = new Weapon();
+
 		Zombie zomA = new Zombie(10);
 
 		yataA.setHp(20);
 		yataB.setHp(20);
 		zomA.setHp(20);
+
+		fujii.setHP(20);
+		ito.setHP(20);
 
 		for(;;){
 			System.out.println("ゾンビAが八咫烏Bに攻撃");
@@ -20,5 +28,22 @@ public class Main {
 				return;
 			}
 		}
+
+		System.out.println("藤井が八咫烏Ｂを攻撃");
+		fujii.attack(yataB);
+		System.out.println("藤井が剣を装備");
+		fujii.setWeapon(w);
+		System.out.println("藤井が八咫烏Ｂを攻撃");
+		fujii.attack(yataB);
+		for(;;){
+			System.out.println("ゾンビAが伊藤に攻撃");
+			zomA.attack(ito);
+			//死んだらおわり
+			if(ito.getHp() <= 0){
+				System.out.println("伊藤 死亡");
+				return;
+			}
+		}
+
 	}
 }
