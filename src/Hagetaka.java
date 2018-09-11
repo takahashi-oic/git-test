b9ca5fcf74515e17e64f025b35118a247b9c8540
fujinawa <akaguroiiro@gmail.com>
2018-09-11 10:16:49 +0900

    up Hagetaka

---
+++ src/Hagetaka.java
@@ -0,0 +1,31 @@
+/**
+*
+* @author fujinawa
+*
+*/
+public class Hagetaka extends Monster implements Bird {
+
+	public void attack(Monster m) {
+		m.setHp(m.getHp() - 5);
+		System.out.println("5のダメージ");
+	}
+
+	public void attack(Character c) {
+		c.setHp(c.getHp() - 5);
+		System.out.println("5のダメージ");
+	}
+
+	public void run() {}
+	public void fly() {}
+
+	public void dead(){
+		System.out.println("ハゲタカは地に落ちた");
+	}
+
+	Hagetaka(){}
+
+	Hagetaka(int hp){
+		super.setHp(hp);
+	}
+
+}
