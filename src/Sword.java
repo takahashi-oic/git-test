f773c1faed27feb686cbdd1890d8274d7bea9b4b
koyama <kym1290kh@gmail.com>
2018-09-11 09:33:54 +0900

    update sword,main

--- src/Sword.java
+++ src/Sword.java
@@ -6,6 +6,7 @@
 public class Sword extends Weapon {
 	public void attack(Monster m) {
 		m.setHp(m.getHp() - this.getPower());
+		System.out.println(this.getPower() + "のダメージ");
 	}
 
 }
--- src/Wand.java
+++ src/Wand.java
@@ -6,5 +6,6 @@
 public class Wand extends Weapon {
 	public void attack(Monster m) {
 		m.setHp(m.getHp() - this.getPower());
+
 	}
 }
