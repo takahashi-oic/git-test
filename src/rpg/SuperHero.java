56d8c5ede426de32909543c68aec52b062dec3bb
takahashi <16110013@14DELL-59.OIC_LAN1>
2018-09-13 13:55:51 +0900

    SuperHero update

--- src/rpg/SuperHero.java
+++ src/rpg/SuperHero.java
@@ -13,7 +13,12 @@ public class SuperHero extends Hero {
 		return super.attack(c);
 	}
 	public boolean attack( Monster m ) {
-		super.attack(m);
-		return super.attack(m);
+		//1回目の攻撃
+				if(super.attack(m) == false){
+					//死んでいる場合は2回目に行かずにreturn
+					return false;
+				}
+				//2回目の攻撃 結果をreturn
+				return super.attack(m);
 	}
 }
